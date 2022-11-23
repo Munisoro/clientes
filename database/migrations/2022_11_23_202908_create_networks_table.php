e50f987bb0d2b58e5be9eb69b6d28c0299d7bd0e
Munisoro <munisoro@gmail.com>
2022-11-23 16:46:39 -0400

    Creando Migrations

---
+++ database/migrations/2022_11_23_202908_create_networks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNetworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('networks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('red');
+            $table->string('cidr');
+            $table->string('tipo');
+            $table->foreignId('router_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ipv4networks');
+    }
+}
---
+++ database/migrations/2022_11_23_203750_create_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->float('precio');
+            $table->string('velocidad');
+            $table->string('limit_at');
+            //$table->string('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
